3ec2e9aebc3ecd2bb3d4fedfc1fb3b6834d0dc1e
unknown <londonvayavong@gmail.com>
2018-02-04 19:11:03 -0800

    added directories, files and server is listening

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+drop database if exists burger_db;
+
+create database burger_db;
+
+USE burger_db;
+
+drop table if exists burgers;
+
+create table burgers
+(
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR (100) NOT NULL,
+devoured boolean default false,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('cheeseburger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('hamburger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('pizzaburger');
